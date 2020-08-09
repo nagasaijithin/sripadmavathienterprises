24670d0678c6a9910fb9e3e2957bf933227c18fb
nagasaijithin <nagasaijithin1@gmail.com>
2020-08-10 02:10:03 +0530

    first commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 import "../scss/main.scss";
+
+const scrollTopBtn = document.querySelector(".scroll-top-btn");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    // scrollTopBtn.style.display = "block";
+    scrollTopBtn.classList.add("slide-in");
+  } else {
+    scrollTopBtn.classList.remove("slide-in");
+    // scrollTopBtn.style.display = "none";
+  }
+}
+
+scrollTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
