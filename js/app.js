2f4cf2193100733aa197dc847079cc50375337c6
nagasaijithin <nagasaijithin1@gmail.com>
2020-08-10 20:14:21 +0530

    comptd the page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 import "../scss/main.scss";
-
+const intoText = document.querySelector("#poster-content-ani-wapper").children;
+let tl = gsap.timeline();
+Array.from(intoText).forEach((element) => {
+  tl.to(element, 0.4, {
+    y: -20,
+    opacity: 1,
+    ease: "power4.out",
+  });
+});
+/// all about scrolling
 const scrollTopBtn = document.querySelector(".scroll-top-btn");
 window.onscroll = function () {
   scrollFunction();
@@ -18,4 +27,44 @@ function scrollFunction() {
 scrollTopBtn.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+});
+
+/// mobile nav functinality
+const handbargermenu = document.querySelector(".hand-burger-menu");
+const handbargermenuShower = document.querySelector(".main-nav-for-mobile");
+const mobilenavlinks = document.querySelectorAll(".main-nav-for-mobile a");
+mobilenavlinks.forEach((each) => {
+  each.addEventListener("click", () => {
+    handbargermenu.classList.remove("clicked");
+    handbargermenu.innerHTML = "|||";
+    gsap.to(handbargermenuShower, 0.3, {
+      bottom: "100%",
+      ease: "power4.out",
+    });
+  });
+});
+handbargermenu.addEventListener("click", () => {
+  if (!handbargermenu.classList.contains("clicked")) {
+    let tl = gsap.timeline();
+    handbargermenu.classList.add("clicked");
+    tl.to(handbargermenuShower, 0.5, {
+      bottom: "0%",
+      ease: "power4.out",
+    });
+    mobilenavlinks.forEach((each) => {
+      tl.to(each, 0.3, {
+        y: -20,
+        opacity: 1,
+        duration: 0.3,
+      });
+    });
+    handbargermenu.innerHTML = "X";
+  } else {
+    handbargermenu.classList.remove("clicked");
+    handbargermenu.innerHTML = "|||";
+    gsap.to(handbargermenuShower, 0.4, {
+      bottom: "100%",
+      ease: "power4.out",
+    });
+  }
 });
